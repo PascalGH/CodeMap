9c67f1ff16ba33159d604219a76a7eb6b68bcfb2
PascalGH <pascaldutrieux@gmail.com>
2019-01-13 10:53:54 +0100

    Completed first version of decode and write to file.

--- Blocks.py
+++ Blocks.py
@@ -18,15 +18,40 @@ class block:
         self.content = content
         self.blocks = []
         self.comments = []
+        self.code = []
+        # print(f'new block: ', name, ',', indent)
 
-    def addblock(self, name, indent, content):
-        self.newblock = block(name, indent, content)
-        self.blocks.append(self.newblock)
+    def display(self):
+        print(f' ' * self.indent * myindent, self.name)
+        for i in self.comments:
+            print(f'  ' * self.indent * myindent, mycomment, ' ',i)
+        for i in self.code:
+            print(f'  ' * self.indent * myindent, ' ',i)
+        for i in self.blocks:
+            i.display()
 
-    def analyse(self):
+    def save(self,file):
+        cursor = ' ' * self.indent * myindent
+        file.write('--------- Block\n')
+        file.write(cursor + self.name + '\n')
+        file.write('-------------------\n')
+        file.write('--------- Comments\n')
+        for i in self.comments:
+            file.write(cursor + '# ' + i + '\n')
+        file.write('-------------------\n')
+        file.write('--------- Code\n')
+        for i in self.code:
+            file.write(cursor + i + '\n')
+        file.write('-------------------\n')
+        for i in self.blocks:
+            i.save(file)
+
+
+    def decode(self):
         indent = 0
-        for line in self.content:
-            print(line)
+        while len(self.content) != 0:
+            line = self.content.pop(0)
+            self.code.append(line)
             sub_line = line.split(mycomment)
             if sub_line[0] != '':
                 term = sub_line[0].split()[0].lower()
@@ -35,27 +60,25 @@ class block:
                         left = list((re.findall(r'\s+', line)))
                         nb_chars = len(left[0])
                         if nb_chars == 1:
-                            indent = 0
-                        else:
-                            indent = int(nb_chars / myindent)
-                            # Here we create a new block below
-                            self.content = self.content.pop(0)
-                            self.addblock('block', indent, self.content)
-                            self.blocks[len(self.blocks) - 1].analyse()
-                        break
+                            nb_chars = 0
+                        indent = int(nb_chars / myindent)
+                        # Here we create a new block below
+                        newblock = block(term, indent + 1, self.content)
+                        self.blocks.append(newblock)
+                        newblock.decode()
                 if len(sub_line) > 1:
-                    # print(f'Comment: ',sub_line[1])
                     self.comments.append(sub_line[1])
             else:
                 if len(sub_line) > 1:
-                    # print(f'Comment: ',sub_line[1])
                     self.comments.append(sub_line[1])
-        self.content = self.content.pop(0)
-        print(self.content)
         return self.content
 
 f = open("Analyser.py","r")
 content = f.read().split(myeol)
 f.close()
 main_block = block(name = 'Main', indent = 0, content = content)
-content = main_block.analyse()
+content = main_block.decode()
+#main_block.display()
+f = open("Analyser.txt","w")
+main_block.save(f)
+f.close()
